3f1cdfc0a9e2707749d5d8d01893082137d52baf
Ama Liyanage <liyaniama@gmail.com>
2018-07-05 09:50:50 -0400

    Added styling to personal info and pregnancy info
    I also added some styling to the title of pick package and added the background image.

--- MM_App/src/Components/PersonalInfo.js
+++ MM_App/src/Components/PersonalInfo.js
@@ -1,25 +1,41 @@
 import firebase from 'firebase';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Picker, Image, } from 'react-native';
-import ExTextInput from './common/ExTextInput';
+import {
+  View,
+  Text,
+  ScrollView,
+  Picker,
+  Image,
+  PixelRatio,
+  TextInput
+} from 'react-native';
+
 import Button from './common/Button';
+
+let FONT_BACK_LABEL = 18;
+
+if (PixelRatio.get() <= 2) {
+  FONT_BACK_LABEL = 14;
+}
 
 const backgroundpic = require('./../../pictures/BackgroundForPages.jpg');
 // need to add input checking
 
 class PersonalInfo extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        age: '',
-        ethnicity: '0',
-        first_name: '',
-        last_name: '',
-        marital_status: '0',
-        phone_number: '',
-        postal_code: '',
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: '',
+      ethnicity: '0',
+      first_name: '',
+      last_name: '',
+      marital_status: 0,
+      phone_number: '',
+      postal_code: '',
+    };
   }
+
   componentWillMount() {
       const { currentUser } = firebase.auth();
       //pull the info and pass it to payload
@@ -31,21 +47,22 @@ class PersonalInfo extends Component {
             this.setState(snapshot.val());
             }
           });
-      }
+  }
 
-      Registration2Submit(values) {
-        const { currentUser } = firebase.auth();
-          firebase
-            .database()
-            .ref(`/users/${currentUser.uid}/userinfo`)
-            .update({ ...values })
-            .then(() => {
-                this.props.navigation.navigate('PregnancyInfo');
-            })
-            .catch(err => {
-              console.log(err);
-            });
-        }
+  Registration2Submit(values) {
+    const { currentUser } = firebase.auth();
+      firebase
+        .database()
+        .ref(`/users/${currentUser.uid}/userinfo`)
+        .update({ ...values })
+        .then(() => {
+            this.props.navigation.navigate('PregnancyInfo');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+
 
   render() {
     return (
@@ -58,133 +75,139 @@ class PersonalInfo extends Component {
             <ScrollView>
                 <Text style={styles.titleLabelStyle}>PERSONAL INFO</Text>
 
-                <ExTextInput
-                    name='First Name'
-                    textChange={(text) => {
+                <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={(text) => {
                         this.setState({ first_name: text });
                       }}
-                    capital='words'
-                    correct={false}
+                    autoCapitalize='words'
                     value={this.state.first_name}
-                    holder={'John'}
+                    placeholder='FIRST NAME'
                 />
 
-                <ExTextInput
+                <TextInput
+                    style={styles.inputStyle}
                     name='Last Name'
-                    textChange={(text) => {
+                    onChangeText={(text) => {
                         this.setState({ last_name: text });
                       }}
-                    capital='words'
-                    correct={false}
+                    autoCapitalize='words'
                     value={this.state.last_name}
-                    holder={'Smith'}
+                    placeholder='LAST NAME'
                 />
 
-                <ExTextInput
-                    name='Phone Number'
-                    textChange={(text) => {
+                <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={(text) => {
                         this.setState({ phone_number: text });
                       }}
-                    correct={false}
                     value={this.state.phone_number}
                     keyboard='phone-pad'
-                    holder={'5555555555'}
-
+                    autoCapitalize='words'
+                    placeholder='PHONE NUMBER'
                 />
 
-                <ExTextInput
-                    name='Postal Code'
-                    textChange={(text) => {
+                <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={(text) => {
                         this.setState({ postal_code: text });
                       }}
-                    capital='characters'
-                    correct={false}
+                    autoCapitalize='characters'
                     value={this.state.postal_code}
-                    holder={'A1B2C3'}
+                    placeholder='POSTAL CODE'
                 />
 
-                <ExTextInput
-                    name='Age'
-                    textChange={(text) => {
+                <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={(text) => {
                         this.setState({ age: text });
                       }}
-                    correct={false}
                     value={this.state.age}
                     keyboard='numeric'
+                    placeholder='AGE'
                 />
 
-                <Picker
-                    selectedValue={this.state.ethnicity}
-                    onValueChange={(value) => {
-                        this.setState({ ethnicity: value });
-                     }}
-                >
-                    <Picker.Item
-                        label="Aboriginal (Inuit, Métis, North American Indian)"
-                        value="0"
-                    />
-                    <Picker.Item
-                        label="Arab/West Asian"
-                        value="1"
-                    />
-                    <Picker.Item
-                        label="Black (e.g., African, Haitian, Jamaican, Somali)"
-                        value="2"
-                    />
-                    <Picker.Item
-                        label="Chinese"
-                        value="3"
-                    />
-                    <Picker.Item
-                        label="Filipino"
-                        value="4"
-                    />
-                    <Picker.Item
-                        label="Japanese"
-                        value="5"
-                    />
-                    <Picker.Item
-                        label="Korean"
-                        value="6"
-                    />
-                    <Picker.Item
-                        label="Latin American"
-                        value="7"
-                    />
-                    <Picker.Item
-                        label="South Asian"
-                        value="8"
-                    />
-                    <Picker.Item
-                        label="South East Asian"
-                        value="9"
-                    />
-                    <Picker.Item
-                        label="Caucasian" value="10"
-                    />
-                    <Picker.Item
-                        label="Other"
-                        value="11"
-                    />
-                </Picker>
-
-                <Picker
-                    selectedValue={this.state.marital_status}
-                    onValueChange={(value) => {
+                <View style={styles.itemContainer}>
+                  <Text style={styles.subTitleStyle}> MARITAL STATUS </Text>
+                  <SegmentedControlTab
+                    activeTabStyle={styles.activeTabStyle}
+                    tabStyle={styles.tabStyle}
+                    tabTextStyle={styles.tabTextStyle}
+                    values={['Single', 'Married', 'Divorced']}
+                    selectedIndex={this.state.marital_status}
+                    onTabPress={(value) => {
                         this.setState({ marital_status: value });
                      }}
-                >
-                <Picker.Item label="Single" value="1" />
-                <Picker.Item label="Married" value="2" />
-                <Picker.Item label="Divorced" value="3" />
-                </Picker>
+                  />
+                </View>
 
-                <Button
-                    onPress={() => { this.Registration2Submit(this.state); }}
-                >
-                    Save and Continue
-                </Button>
+                <View style={styles.itemContainer}>
+                  <Text style={styles.subTitleStyle}> ETHNICITY </Text>
+                  <Picker
+                      itemStyle={styles.tabTextStyle}
+                      style={styles.pickerStyle}
+                      selectedValue={this.state.ethnicity}
+                      onValueChange={(value) => {
+                          this.setState({ ethnicity: value });
+                       }}
+                  >
+                      <Picker.Item
+                          label="Aboriginal (Inuit, Métis, North American Indian)"
+                          value="0"
+                      />
+                      <Picker.Item
+                          label="Arab/West Asian"
+                          value="1"
+                      />
+                      <Picker.Item
+                          label="Black (e.g., African, Haitian, Jamaican, Somali)"
+                          value="2"
+                      />
+                      <Picker.Item
+                          label="Chinese"
+                          value="3"
+                      />
+                      <Picker.Item
+                          label="Filipino"
+                          value="4"
+                      />
+                      <Picker.Item
+                          label="Japanese"
+                          value="5"
+                      />
+                      <Picker.Item
+                          label="Korean"
+                          value="6"
+                      />
+                      <Picker.Item
+                          label="Latin American"
+                          value="7"
+                      />
+                      <Picker.Item
+                          label="South Asian"
+                          value="8"
+                      />
+                      <Picker.Item
+                          label="South East Asian"
+                          value="9"
+                      />
+                      <Picker.Item
+                          label="Caucasian" value="10"
+                      />
+                      <Picker.Item
+                          label="Other"
+                          value="11"
+                      />
+                  </Picker>
+                </View>
 
+                <View style={styles.buttonContainer}>
+                  <Button
+                      onPress={() => { this.Registration2Submit(this.state); }}
+                  >
+                      Save and Continue
+                  </Button>
+                </View>
             </ScrollView>
         </View>
     );
@@ -193,15 +216,60 @@ class PersonalInfo extends Component {
 
 const styles = {
   titleLabelStyle: {
-    padding: 7,
-    paddingLeft: 10,
+    padding: 8,
+    paddingLeft: 16,
+    marginBottom: 8,
     fontSize: 20,
     color: '#00bbdd',
-    },
-    pickerStyle: {
-      paddingLeft: 30,
-      paddingRight: 30,
-    }
+    fontFamily: 'fjalla-one',
+  },
+  pickerStyle: {
+    paddingRight: 32,
+    paddingLeft: 32,
+  },
+  inputStyle: {
+    paddingRight: 16,
+    paddingBottom: 8,
+    marginLeft: 32,
+    marginRight: 32,
+    marginBottom: 24,
+    color: '#000',
+    fontSize: FONT_BACK_LABEL,
+    fontFamily: 'fjalla-one',
+    lineHeight: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: '#ddd'
+  },
+  itemContainer: {
+    marginLeft: 32,
+    marginRight: 32,
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  activeTabStyle: {
+    backgroundColor: '#852053',
+    opacity: 0.7
+  },
+  tabStyle: {
+    borderColor: '#852053',
+    backgroundColor: 'transparent',
+    opacity: 0.7,
+  },
+  tabTextStyle: {
+    color: 'grey',
+    fontFamily: 'fjalla-one'
+  },
+  subTitleStyle: {
+    paddingBottom: 8,
+    fontSize: FONT_BACK_LABEL,
+    fontFamily: 'fjalla-one',
+    opacity: 0.8
+  },
+  buttonContainer: {
+    marginLeft: 32,
+    marginRight: 32,
+    marginBottom: 32
+  }
 };
 
 export default PersonalInfo;
--- MM_App/src/Components/PregnancyInfo.js
+++ MM_App/src/Components/PregnancyInfo.js
@@ -1,15 +1,23 @@
 import firebase from 'firebase';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
 import React, { Component } from 'react';
 import {
     View,
     Text,
     ScrollView,
-    Picker,
     Image,
     Platform,
     DatePickerIOS,
+    PixelRatio,
+    TextInput
     } from 'react-native';
 import Button from './common/Button';
+
+let FONT_BACK_LABEL = 18;
+
+if (PixelRatio.get() <= 2) {
+  FONT_BACK_LABEL = 14;
+}
 
 const backgroundpic = require('./../../pictures/BackgroundForPages.jpg');
 
@@ -34,10 +42,10 @@ class PregnancyInfo extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        current_children: '0',
+        current_children: '',
         expecting_date: new Date(),
-        number_children: '0',
-        trimester: '0'
+        number_children: '',
+        trimester: 0
       };
   }
 
@@ -77,62 +85,59 @@ class PregnancyInfo extends Component {
             <View style={{ paddingTop: 10 }}>
                 <Image style={[{ position: 'absolute' }]} source={backgroundpic} />
             </View>
+
             <ScrollView>
                 <Text style={styles.titleLabelStyle}>PREGNANCY</Text>
 
-                <Picker
-                    selectedValue={this.state.trimester}
-                    onValueChange={(value) => {
-                        this.setState({ trimester: value });
-                     }}
-                >
-                    <Picker.Item label="First Trimester" value="0" />
-                    <Picker.Item label="Second Trimester" value="1" />
-                    <Picker.Item label="Third Trimester" value="2" />
-                </Picker>
-
-                <Picker
-                    selectedValue={this.state.number_children}
-                    onValueChange={(value) => {
-                        this.setState({ number_children: value });
-                     }}
-                >
-                    <Picker.Item label="0" value="0" />
-                    <Picker.Item label="1" value="1" />
-                    <Picker.Item label="2" value="2" />
-                    <Picker.Item label="3" value="3" />
-                    <Picker.Item label="4" value="4" />
-                    <Picker.Item label="5" value="5" />
-                    <Picker.Item label="6" value="6" />
-                </Picker>
-
-                <Picker
-                    selectedValue={this.state.current_children}
-                    onValueChange={(value) => {
-                        this.setState({ current_children: value });
-                     }}
-                >
-                    <Picker.Item label="0" value="0" />
-                    <Picker.Item label="1" value="1" />
-                    <Picker.Item label="2" value="2" />
-                    <Picker.Item label="3" value="3" />
-                    <Picker.Item label="4" value="4" />
-                    <Picker.Item label="5" value="5" />
-                    <Picker.Item label="6" value="6" />
-                </Picker>
-
-                <NewFormDatePicker
-                    style={styles.pickerStyle}
-                    date={new Date(this.state.expecting_date)}
-                    onDateChange={(value) => this.setState({ expecting_date: value })}
-                    mode='date'
+                <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={(text) => {
+                        this.setState({ current_children: text });
+                      }}
+                    keyboard='numeric'
+                    value={this.state.current_children}
+                    placeholder='NUMBER OF CURRENT CHILDREN'
                 />
 
-                <Button
-                    onPress={() => { this.Registration2Submit(this.state); }}
-                >
-                    Submit and Continue
-                </Button>
+                <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={(text) => {
+                        this.setState({ number_children: text });
+                      }}
+                    keyboard='numeric'
+                    value={this.state.number_children}
+                    placeholder='EXPECTING NUMBER OF CHILDREN'
+                />
+
+                <View style={styles.itemContainer}>
+                  <Text style={styles.subTitleStyle}>TRIMESTER</Text>
+                  <SegmentedControlTab
+                    activeTabStyle={styles.activeTabStyle}
+                    tabStyle={styles.tabStyle}
+                    tabTextStyle={styles.tabTextStyle}
+                    values={['First', 'Second', 'Third']}
+                    selectedIndex={this.state.trimester}
+                    onTabPress={(value) => {
+                        this.setState({ trimester: value });
+                     }}
+                  />
+                </View>
+
+                <View style={styles.itemContainer}>
+                  <Text style={styles.subTitleStyle}> EXPECTING DATE </Text>
+                    <NewFormDatePicker
+                        date={new Date(this.state.expecting_date)}
+                        onDateChange={(value) => this.setState({ expecting_date: value })}
+                        mode='date'
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                  <Button
+                      onPress={() => { this.Registration2Submit(this.state); }}
+                  >
+                      Submit and Continue
+                  </Button>
+                </View>
             </ScrollView>
         </View>
     );
@@ -141,15 +146,61 @@ class PregnancyInfo extends Component {
 
 const styles = {
   titleLabelStyle: {
-    padding: 7,
-    paddingLeft: 10,
+    padding: 8,
+    paddingLeft: 16,
+    marginBottom: 8,
     fontSize: 20,
     color: '#00bbdd',
-    },
-    pickerStyle: {
-      paddingLeft: 30,
-      paddingRight: 30,
-    }
+    fontFamily: 'fjalla-one',
+  },
+  inputStyle: {
+    paddingRight: 16,
+    paddingBottom: 8,
+    marginLeft: 32,
+    marginRight: 32,
+    marginBottom: 24,
+    color: '#000',
+    fontSize: FONT_BACK_LABEL,
+    fontFamily: 'fjalla-one',
+    lineHeight: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: '#ddd'
+  },
+  itemContainer: {
+    marginLeft: 32,
+    marginRight: 32,
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  datePickerContainer: {
+    marginTop: 16,
+    marginLeft: 32,
+    marginRight: 32
+  },
+  activeTabStyle: {
+    backgroundColor: '#852053',
+    opacity: 0.7
+  },
+  tabStyle: {
+    borderColor: '#852053',
+    backgroundColor: 'transparent',
+    opacity: 0.7,
+  },
+  tabTextStyle: {
+    color: 'grey',
+    fontFamily: 'fjalla-one'
+  },
+  subTitleStyle: {
+    paddingBottom: 8,
+    fontSize: FONT_BACK_LABEL,
+    fontFamily: 'fjalla-one',
+    opacity: 0.8
+  },
+  buttonContainer: {
+    marginLeft: 32,
+    marginRight: 32,
+    marginBottom: 32
+  }
 };
 
 export default PregnancyInfo;
